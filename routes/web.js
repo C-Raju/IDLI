943f407c67e0a029616536011921c016b995bc20
unknown <chandrashekharraju306@gmail.com>
2022-03-20 20:21:03 +0530

    add orders table

--- routes/web.js
+++ routes/web.js
@@ -1,14 +1,33 @@
 const homeController = require('../app/http/controller/homeController')
 const authController = require('../app/http/controller/authController')
 const cartController = require('../app/http/controller/customers/cartController')
+const orderController = require('../app/http/controller/customers/orderController')
+const AdminOrderController = require('../app/http/controller/admin/orderController')
+
+// Middlewares 
+const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+const admin = require('../app/http/middleware/admin')
+
 
 function initRoutes(app) {
     app.get('/', homeController().index)
-    app.get('/login', authController().login)
-    app.get('/register', authController().register)
+
+    app.get('/login', guest, authController().login)
+    app.post('/login', authController().postLogin)
+    app.get('/register', guest, authController().register)
+    app.post('/register', authController().postRegister)
+    app.post('/logout', authController().logout)
 
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update)
+
+    app.post('/orders',  auth, orderController().store)
+    app.get('/customer/orders',  auth, orderController().index)
+
+    //Admin routes
+    app.get('/admin/orders', auth, AdminOrderController().index)
+    
 }
  
 module.exports = initRoutes
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose')
 const session = require('express-session')
 const flash = require('express-flash')
 const MongoDbstore = require('connect-mongo')
+const passport = require('passport')
 
 //Database connection
 mongoose.Promise = global.Promise;
@@ -25,7 +26,10 @@ mongoose.connect('mongodb://localhost:27017/idli', {
         console.log('Error in DB connection: ' + err)
     }
 });
+
+ 
 //session store
+//in progress later
 
 //session config
 app.use(session({
@@ -36,18 +40,25 @@ app.use(session({
     }),
     saveUninitialized: false,
     cookie: { maxAge: 1000 * 60 * 60 * 24  }
-  }));
+  }))
+
+  // Passport config
+ const passportInit = require('./app/config/passport')
+ passportInit(passport)
+ app.use(passport.initialize())
+ app.use(passport.session())
 
 app.use(flash());
 
-
 // assets
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 //global middleware
 app.use((req, res, next) => {
     res.locals.session = req.session
+    res.locals.user = req.user
     next()
 })
 // set template engine
